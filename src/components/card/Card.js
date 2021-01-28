46cc806efaa785bc6817d2fb9bebf8ad988f479f
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2021-01-28 13:54:19 -0600

    react-projects-chapter1
    Initial code changes for this branch

---
+++ src/components/card/Card.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Card = () => ( <h1>MovieList</h1> )
+export default Card;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './containers/List'
+
+const App = () => {
+    return (
+      <List/>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
