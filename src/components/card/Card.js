cc9f69f53c4f302ddaa2a1f69ef4fbd12fb7a1db
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2021-01-30 13:32:09 -0600

    react-projects-chapter1
    Implemented functionality to load movies list

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,4 +1,13 @@
 import React from 'react';
 
-const Card = () => ( <h1>MovieList</h1> )
-export default Card;
+export default function Card({ movie }){
+    const { ranking, title, year, distributor, amount, img: { src, alt }} = movie;
+    return (
+        <div>
+            <h2>{`${ranking}, ${title}, ${year}`}</h2>
+            <img src={src} alt={alt} width='200'/>
+            <p>{`Distributor: ${distributor}`}</p>
+            <p>{`Amount ${amount}`}</p>
+        </div>
+    )
+}
--- src/containers/List.js
+++ src/containers/List.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '../components/card/Card';
 
 export default function List() {
+    const [ movieSummary, setMovieSummary ] = useState({ movies: [], loading: true });
+    const { movies, loading } = movieSummary;
+
+    useEffect(async () => {
+        const result = await fetch('../../src/assets/data.json');
+        if (!!result) {
+            setMovieSummary({ movies: await result.json(), loading: false });
+        }
+    })
     return (
-        <Card/>
+        <div>
+          { loading && <div>Loading...</div>}
+          { movies && movies.map(movie => <Card key={movie.id} movie={movie}/>) }
+        </div>
     )
 }
